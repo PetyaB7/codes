e098aa49f8282bb06fdc40bf07c770feaa81fdf1
PetyaB7 <147176478+PetyaB7@users.noreply.github.com>
2024-01-12 21:42:42 +0200

    Create 11,01.py

---
+++ 11,01.py
@@ -0,0 +1,7 @@
+names = input().split(' ')
+n = int(input())
+player = 0
+while len(names) > 1:
+    player = ((player + n - 1) % len(names))
+    print(f"Removed: {names.pop(player)}")
+print(f'Last is {names[0]}')
